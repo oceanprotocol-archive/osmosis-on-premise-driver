6ad8e8ddc4dc302c889d9475ab6fdebae7452445
eruizgar91 <enrique@oceanprotocol.com>
2019-01-22 15:51:46 +0100

    Bumpversion

--- setup.py
+++ setup.py
@@ -83,6 +83,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/oceanprotocol/osmosis-on-premise-driver',
-    version='0.0.3',
+    version='0.0.4',
     zip_safe=False,
 )
